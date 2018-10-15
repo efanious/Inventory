f610b91c866de4becf14639766ce0219ee642cd5
Efana <usuaefana@gmail.com>
2018-10-15 21:35:24 +0100

    First version of inventory app

--- app/src/main/java/com/android/inventory/EditorActivity.java
+++ app/src/main/java/com/android/inventory/EditorActivity.java
@@ -1,13 +1,90 @@
 package com.android.inventory;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.inventory.data.ProductContract.ProductEntry;
+import com.android.inventory.data.ProductDbHelper;
 
 public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the product's name */
+    private EditText mProductNameEditText;
+
+    /** EditText field to enter the product's quantity */
+    private EditText mQuantityEditText;
+
+    /** EditText field to enter the product's price */
+    private EditText mPriceEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        mProductNameEditText = (EditText) findViewById(R.id.edit_product_name);
+        mQuantityEditText = (EditText) findViewById(R.id.edit_product_quantity);
+        mPriceEditText = (EditText) findViewById(R.id.edit_price);
+    }
+
+    //Insert product from edit text
+    private void insertProduct(){
+
+        // Read from input field
+        String productNameString = mProductNameEditText.getText().toString().trim();
+        String quantityString = mQuantityEditText.getText().toString().trim();
+        int quantity = Integer.parseInt(quantityString);
+        String priceString = mPriceEditText.getText().toString().trim();
+        int price = Integer.parseInt(priceString);
+
+        ProductDbHelper mDbHelper = new ProductDbHelper(this);
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, productNameString);
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, quantity);;
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, price);
+
+        long newRowId = db.insert(ProductEntry.TABLE_NAME, null,values);
+
+        if(newRowId == -1) {
+            Toast.makeText(this,"Error with saving product", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Product saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                insertProduct();
+                finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/android/inventory/InventoryActivity.java
+++ app/src/main/java/com/android/inventory/InventoryActivity.java
@@ -38,24 +38,78 @@ public class InventoryActivity extends AppCompatActivity {
         });
 
         mDbHelper = new ProductDbHelper(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         displayDatabaseInfo();
     }
+
+    // With table
 
     private void displayDatabaseInfo() {
 
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
 
-        Cursor cursor = db.rawQuery("SELECT * FROM " + ProductEntry.TABLE_NAME, null);
-        try{
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_PRICE
+        };
 
-            TextView displayView = (TextView) findViewById(R.id.text_view_products);
-            displayView.setText("Number of rows in products database table: " + cursor.getCount());
+        Cursor cursor = db.query(
+                ProductEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        TextView displayView = (TextView) findViewById(R.id.text_view_products);
+        try {
+
+
+            displayView.setText("The inventory table contains " + cursor.getCount() + " products.\n\n");
+            displayView.append(ProductEntry._ID + " - " +
+                    ProductEntry.COLUMN_PRODUCT_NAME + " - " +
+                    ProductEntry.COLUMN_PRODUCT_QUANTITY + " - " +
+                    ProductEntry.COLUMN_PRODUCT_PRICE + "\n"
+            );
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(ProductEntry._ID);
+            int productNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
+            int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+            int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentProductName = cursor.getString(productNameColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+                int currentPrice = cursor.getInt(priceColumnIndex);
+
+                //Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentProductName + " - " +
+                        currentQuantity + " - " +
+                        currentPrice));
+            }
+
+
         } finally {
-
             cursor.close();
         }
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
